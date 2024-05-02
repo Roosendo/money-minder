2cfcce5d30ae8e6842529609c28de9f80ae31eab
Rosendo <grosendoh73@gmail.com>
2024-05-02 16:57:04 -0600

    Add auth-astro integration, update npm dependencies and add lib alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
+import auth from 'auth-astro'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind()]
+	output: 'server',
+	adapter: vercel(),
+	integrations: [tailwind(), auth()],
 })
