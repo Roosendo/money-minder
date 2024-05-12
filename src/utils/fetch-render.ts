2b5b4fdda7496f6793edab30fdc1f2ae1c4880b2
Rosendo <grosendoh73@gmail.com>
2024-05-12 17:16:56 -0600

    Add form component for both transactions, improve logic for data fetching

---
+++ src/utils/fetch-render.ts
@@ -0,0 +1,34 @@
+import { renderTable } from '@utils/render-table'
+
+interface Transaction {
+  entry_id: number
+  user_email: string
+  amount: number
+  description: string
+  category: string
+  date: string
+}
+
+export const fetchData = async (url: string): Promise<Transaction[]> => {
+  if (url !== 'entries' && url !== 'exits') {
+    return []
+  }
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  }
+  const response = await fetch(`/api/get-${url}`, requestOptions)
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok')
+  }
+
+  const transaction: Transaction[] = await response.json()
+
+  return transaction
+}
+
+export const renderData = async (data: Transaction[], element: HTMLElement) => {
+  const tbody = renderTable(data)
+  element.innerHTML = tbody
+}
