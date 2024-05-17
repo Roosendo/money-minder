5b32d3e38fc7004eb02388aef537439cfb7d992b
Rosendo <grosendoh73@gmail.com>
2024-05-17 11:01:38 -0600

    Create API endpoint for financial summary by month and year

---
+++ src/pages/api/[month]/[year]/financial-summary.ts
@@ -0,0 +1,45 @@
+import type { APIRoute } from 'astro'
+import { createClient } from '@libsql/client'
+import { getSession } from 'auth-astro/server'
+
+const client = createClient({
+  url: import.meta.env.TURSO_DATABASE_URL ?? '',
+  authToken: import.meta.env.TURSO_AUTH_TOKEN ?? '',
+})
+
+// GET -> api/[month]/[year]/financial-summary
+// []* replace with the month and year from the user
+// JSON -> {"totalEntries":150.09,"totalExits":35.99}
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const month = params.month
+  const year = params.year
+  const session = await getSession(request)
+  const email = session?.user?.email
+
+  if (!email) return new Response('Unauthorized', { status: 401 })
+  if (!month || !year) return new Response('Month and year are required', { status: 400 })
+
+  const userEntries = await client.execute({
+    sql: 'SELECT SUM(amount) AS totalEntries FROM money_entries WHERE user_email = ? AND strftime("%m", date) = ? AND strftime("%Y", date) = ?',
+    args: [email, month, year]
+  })
+
+  const userExits = await client.execute({
+    sql: 'SELECT SUM(amount) AS totalExits FROM money_exits WHERE user_email = ? AND strftime("%m", date) = ? AND strftime("%Y", date) = ?',
+    args: [email, month, year]
+  })
+
+  if (userEntries.rows.length === 0) {
+    return new Response('Entries by category not found', { status: 404 })
+  }
+
+  if (userExits.rows.length === 0) {
+    return new Response('Exits by category not found', { status: 404 })
+  }
+
+  const totalEntries = userEntries?.rows[0]?.totalEntries ?? 0
+  const totalExits = userExits?.rows[0]?.totalExits ?? 0
+
+  return new Response(JSON.stringify({ totalEntries, totalExits }), { status: 200 })
+}
