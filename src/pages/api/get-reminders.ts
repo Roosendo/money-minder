e3e1b38c3968c97bac4ded1ee26d0a0505675735
Rosendo <grosendoh73@gmail.com>
2024-05-27 10:44:07 -0600

    ✨ add crud endpoints for reminders

---
+++ src/pages/api/get-reminders.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from 'astro'
+import { getSessionAndClient } from '@config/utils'
+
+// GET -> api/get-reminders
+// JSON -> [{"id":1,"title":"Cuota","description":"A PAGAR","reminder_date":"2024-05-31T08:53","is_completed":0}]
+
+export const GET: APIRoute = async ({ request }) => {
+  const { session, client } = await getSessionAndClient(request)
+  const email = session?.user?.email
+
+  if (!email) return new Response('Unauthorized', { status: 401 })
+
+  const user = await client.execute({
+    sql: 'SELECT id, title, description, reminder_date, is_completed FROM reminders WHERE user_email = ?',
+    args: [email]
+  })
+
+  if (user.rows.length === 0) return new Response('Reminders not found', { status: 404 })
+
+  return new Response(JSON.stringify(user.rows), { status: 200 })
+}
---
+++ src/pages/api/reminders.ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from 'astro'
+import { getSessionAndClient } from '@config/utils'
+import { isValidISODateTime } from '@utils/validation'
+
+interface Reminder {
+  title: string
+  description: string
+  reminderDate: string
+}
+
+// POST -> /api/reminders
+
+export const POST: APIRoute = async ({ request }) => {
+  const { session, client } = await getSessionAndClient(request)
+
+  const body = await request.json()
+  const { title, description, reminderDate } = body as Reminder
+  const email = session?.user?.email
+  const full_name = session?.user?.name
+
+  if (!email || !full_name) return new Response('Unauthorized', { status: 401 })
+  if (!title || !reminderDate) return new Response('Missing required fields', { status: 400 })
+
+  if (!isValidISODateTime(reminderDate)) return new Response('Invalid date format', { status: 400 })
+
+  const user = await client.execute({
+    sql: 'SELECT * FROM users WHERE email = ?',
+    args: [email]
+  })
+
+  if (user.rows.length === 0) {
+    await client.execute({
+      sql: 'INSERT INTO users (email, full_name) VALUES (?, ?)',
+      args: [email, full_name]
+    })
+  }
+
+  await client.execute({
+    sql: 'INSERT INTO reminders (user_email, title, description, reminder_date) VALUES (?, ?, ?, ?)',
+    args: [email, title, description, reminderDate]
+  })
+
+  return new Response(JSON.stringify({
+    message: 'Reminder added successfully'
+  }), { status: 201 })
+}
