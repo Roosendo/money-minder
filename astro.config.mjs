c0aebb495338ae636504d5214d14565026603e9e
Rosendo <grosendoh73@gmail.com>
2024-06-07 13:31:02 -0600

    ⬆️➕ Update dependencies and add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
-import tailwind from '@astrojs/tailwind'
-import auth from 'auth-astro'
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
+import auth from 'auth-astro';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
-	integrations: [tailwind(), auth()],
-	security: {
-		checkOrigin: true
-	}
-})
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind(), auth(), react()],
+  security: {
+    checkOrigin: true
+  }
+});
