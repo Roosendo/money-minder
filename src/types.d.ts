8b22f74f7e07615b3574b13243f1e38c7e66150b
Rosendo <grosendoh73@gmail.com>
2024-06-08 21:14:04 -0600

    🚚 Move types/interfaces to a single file

---
+++ src/types.d.ts
@@ -0,0 +1,46 @@
+export interface Transaction {
+  category: string
+  total: number
+}
+
+export interface Summary {
+  totalEntries: number
+  totalExits: number
+}
+
+export interface Saving {
+  id: number
+  name: string
+  target_amount: number
+  current_amount: number
+  end_date: string
+}
+
+export interface Reminder {
+  id: number
+  title: string
+  description: string
+  reminder_date: string
+}
+
+export interface CashFLow {
+  month: string
+  total_ingresos: number
+  total_egresos: number
+}
+
+export interface FinancialSummary {
+  totalEntries: number | null
+  totalExits: number | null
+}
+
+export interface MainCategories {
+  category: string
+  total: number
+}
+
+export interface RecentTransactions {
+  date: string
+  category: string
+  amount: number
+}
