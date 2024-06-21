3a535d4aad78426cf129d9bbdd1ca61777c73aaf
Rosendo <grosendoh73@gmail.com>
2024-06-21 17:30:53 -0600

    🗑️ delete astro endpoints, now it has its own project

--- src/config/tursoClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@libsql/client'
-
-export const client = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL ?? '',
-  authToken: import.meta.env.TURSO_AUTH_TOKEN ?? ''
-})
--- src/config/utils.ts
+++
@@ -1,7 +0,0 @@
-import { getSession } from 'auth-astro/server'
-import { client } from '@config/tursoClient'
-
-export const getSessionAndClient = async (request: Request) => {
-  const session = await getSession(request)
-  return { session, client }
-}
