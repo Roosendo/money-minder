b785d97993570f3235258b70ec0b738b8b919c71
Rosendo <grosendoh73@gmail.com>
2024-07-12 23:24:58 -0600

    🚀 Init angular migration

--- src/utils/alerts.ts
+++
@@ -1,8 +0,0 @@
-export function showAndHideAlert (alertElement: HTMLDivElement, timeout: number = 5000) {
-  alertElement.classList.remove('hidden')
-  alertElement.classList.add('flex')
-  setTimeout(() => {
-    alertElement.classList.remove('flex')
-    alertElement.classList.add('hidden')
-  }, timeout)
-}
