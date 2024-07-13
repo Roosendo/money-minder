9ac47fcb453989fdc7ed1c14371c4e31e942f6cf
Rosendo <grosendoh73@gmail.com>
2024-07-12 22:55:36 -0600

    🗑️ Remove auth-astro things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import tailwind from '@astrojs/tailwind';
-import auth from 'auth-astro';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), auth(), react()],
+  integrations: [tailwind(), react()],
   security: {
     checkOrigin: true
   }
-});
+})
