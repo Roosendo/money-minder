0122f3a53f1ffd59f696e7ba6939cb00ca49fd86
Rosendo Garcia <grosendoh73@gmail.com>
2024-10-08 16:13:59 -0600

    ➕ Add some dependencies
    they were added: ngrx store, effects and store devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import {
   type ApplicationConfig,
-  provideExperimentalZonelessChangeDetection
+  provideExperimentalZonelessChangeDetection, isDevMode
 } from '@angular/core'
 import { provideRouter, withComponentInputBinding } from '@angular/router'
 
 import { provideHttpClient, withFetch } from '@angular/common/http'
 import { provideClientHydration } from '@angular/platform-browser'
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts'
-import { routes } from './app.routes'
+import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +18,9 @@ export const appConfig: ApplicationConfig = {
     provideExperimentalZonelessChangeDetection(),
     provideHttpClient(withFetch()),
     provideRouter(routes, withComponentInputBinding()),
-    provideCharts(withDefaultRegisterables())
-  ]
+    provideCharts(withDefaultRegisterables()),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 }
